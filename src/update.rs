746ab784e07f78a41d94e6a5a0937ac55d4add58
realTristan <heytristaann@gmail.com>
2022-12-14 15:34:10 -0500

    fixed update command

--- src/update.rs
+++ src/update.rs
@@ -1,4 +1,4 @@
-use std::fs::{self, File};
+use std::fs::File;
 use std::io;
 
 // Initialize the update command
@@ -14,43 +14,29 @@ pub async fn init() {
     let body = resp.text().await;
     let body: String = match body {
         Ok(b) => b,
-        Err(e) => panic!("failed to reach version body {:?}", e),
+        Err(e) => panic!("failed to fetch version body {:?}", e),
     };
 
-    // Create the new executable file
-    let file = File::create("_lat");
+    // Open the current lat executable file
+    let file = File::open("lat");
     let mut file: File = match file {
         Ok(f) => f,
-        Err(e) => panic!("failed to create new version executable. {:?}", e),
+        Err(e) => panic!("failed to open current lat version. {:?}", e),
     };
 
     // Copy the request body bytes to the new file
     let res = io::copy(&mut body.as_bytes(), &mut file);
     let res: u64 = match res {
         Ok(r) => r,
-        Err(copy_error) => match fs::remove_file("_lat") {
-            Ok(_) => panic!(
-                "failed to copy response data to new executable. {:?}",
-                copy_error
-            ),
-            Err(rm_error) => panic!("failed to remove temporary _lat executable. {:?}", rm_error),
-        },
+        Err(e) => panic!("failed to copy response data to lat executable. {:?}", e),
     };
 
-    // If the copy to the temporary _lat executable
-    // was a success...
+    // If the update was a success...
     if res > 0 {
-        match fs::remove_file("lat") {
-            Ok(_) => match fs::rename("_lat", "lat") {
-                Ok(_) => println!("successfully updated lat.cli"),
-                Err(_) => {
-                    panic!("failed to convert _lat to lat. use \"_lat update\" to try again.")
-                }
-            },
-            Err(_) => match fs::remove_file("_lat") {
-                Ok(_) => panic!("failed to remove existing lat file."),
-                Err(_) => panic!("failed to remove existing lat file and temporary lat file. please visit your $PATH to update manually."),
-            },
-        }
+        println!("successfully updated lat.cli")
+    }
+    // Else, if the update was not a success...
+    else {
+        println!("something went wrong... please try updating again.")
     }
 }
